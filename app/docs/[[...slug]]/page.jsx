179a9b5e398339138d2b83dd1474f5cb7ddcd5a3
KolyaFedorenko <kolyafedorenko645@gmail.com>
2026-04-07 16:43:33 +0700

    fix(docs): incorrect column width with MDX content

--- app/docs/[[...slug]]/page.jsx
+++ app/docs/[[...slug]]/page.jsx
@@ -72,7 +72,7 @@ export default async function DocPage({ params }) {
 
   return (
     <div className="flex flex-1 flex-col xl:px-6">
-      <main className="mt-14 max-w-7xl px-5 lg:grid lg:grid-cols-[240px_1fr] lg:gap-10 xl:mx-auto xl:grid-cols-[240px_1fr_240px] xl:px-0">
+      <main className="mt-14 max-w-7xl px-5 lg:grid lg:grid-cols-[240px_1fr] lg:gap-10 xl:mx-auto xl:grid-cols-[240px_720px_240px] xl:px-0">
         <aside className="relative hidden text-sm lg:block">
           <div className="sticky top-14 flex h-[calc(100vh-3.5rem)] w-[240px] overflow-y-auto py-8 xl:flex">
             <DocsSidebar folder={currentFolder} />
@@ -83,7 +83,7 @@ export default async function DocPage({ params }) {
           </div>
         </aside>
         <article className="flex w-full min-w-0 flex-col gap-6 py-8 text-neutral-800 dark:text-neutral-300">
-          <div className="flex w-full flex-col gap-6 md:mx-auto md:max-w-[40rem] xl:max-w-3xl">
+          <div className="flex w-full flex-col gap-6 md:mx-auto md:max-w-[40rem]">
             <div className="flex flex-col gap-2">
               <h1 className="!m-0 !text-3xl font-semibold tracking-tight sm:text-3xl">
                 {doc.title}

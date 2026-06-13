13521e5b7a7634425d7dcab5eca2dda34565ee2a
KolyaFedorenko <kolyafedorenko645@gmail.com>
2026-06-13 20:41:55 +0700

    feat: add sitemap

---
+++ app/sitemap.js
@@ -0,0 +1,49 @@
+import { docs } from '@content';
+
+const BASE_URL = 'https://webstray.com';
+
+export default async function sitemap() {
+  const mainRoutes = [
+    {
+      url: BASE_URL,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 1.0,
+    },
+    {
+      url: `${BASE_URL}/authenticator`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/starlight`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/cli`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+    {
+      url: `${BASE_URL}/docs`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.8,
+    },
+  ];
+
+  const docsRoutes = docs.map((doc) => {
+    return {
+      url: `${BASE_URL}/docs/${doc.slug}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.6,
+    };
+  });
+
+  return [...mainRoutes, ...docsRoutes];
+}
